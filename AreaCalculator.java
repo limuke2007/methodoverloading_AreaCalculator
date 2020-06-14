f75cd4eb252f2cd653b1968d1caad7184a0bb0f0
Limu Ke <62216538+limuke2007@users.noreply.github.com>
2020-06-14 11:19:04 -0500

    add comments on overloading methods

--- AreaCalculator.java
+++ AreaCalculator.java
@@ -1,14 +1,14 @@
 package java_masterclass;
 
 public class AreaCalculator {
-    private static double area(double radius) {
+    private static double area(double radius) {                 // first method 参数(radius)
         if (radius < 0) {
             return -1.0;
         }
         return Math.PI * radius * radius;
     }
 
-    private static double area(double x, double y) {
+    private static double area(double x, double y) {            //  // second method 参数(边长x, y)
         if (x < 0 || y < 0) {
             return -1;
         }
